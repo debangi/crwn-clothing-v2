2c04aecd117166414bd65171322a67a2b14cba7b
debangi <debangiofficial@gmail.com>
2022-05-19 02:30:40 +0530

    updating cart context to cart reducer

--- src/store/categories/category.selector.js
+++ src/store/categories/category.selector.js
@@ -1,14 +1,12 @@
 import { createSelector } from 'reselect';
 
 const selectCategoryReducer = (state) => {
-  console.log('selector 1 fired');
   return state.categories;
 };
 
 export const selectCategories = createSelector(
   [selectCategoryReducer],
   (categoriesSlice) => {
-    console.log('selector 2 fired');
     return categoriesSlice.categories;
   }
 );
@@ -16,7 +14,6 @@ export const selectCategories = createSelector(
 export const selectCategoriesMap = createSelector(
   [selectCategories],
   (categories) => {
-    console.log('selector 3 fired');
     return categories.reduce((acc, category) => {
       const { title, items } = category;
       acc[title.toLowerCase()] = items;
